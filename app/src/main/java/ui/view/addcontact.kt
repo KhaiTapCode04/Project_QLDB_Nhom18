1bde1eabb8842401ad26e81274ccbe4d931fcdc1
thangvn0987 <nguyenminhthangvn0987@gmail.com>
2025-04-30 17:11:13 +0700

    Them view add contact

---
+++ app/src/main/java/ui/view/addcontact.kt
@@ -0,0 +1,107 @@
+package ui.view
+
+// AddContactActivity.kt
+
+
+
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import ui.viewmodel.contact.AddContactViewModel
+
+
+@Composable
+fun AddContactScreen(
+    navController: NavController,
+    viewModel: AddContactViewModel = viewModel()
+) {
+
+    val context = LocalContext.current
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Thêm liên lạc") }
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = viewModel.name.value,
+                onValueChange = { viewModel.name.value = it },
+                label = { Text("Tên") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = viewModel.phone.value,
+                onValueChange = { viewModel.phone.value = it },
+                label = { Text("Số điện thoại") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = viewModel.email.value,
+                onValueChange = { viewModel.email.value = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            /*OutlinedTextField(
+                value = viewModel.birthday.value,
+                onValueChange = { viewModel.birthday.value = it },
+                label = { Text("Ngày sinh") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = viewModel.address.value,
+                onValueChange = { viewModel.address.value = it },
+                label = { Text("Địa chỉ") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = viewModel.note.value,
+                onValueChange = { viewModel.note.value = it },
+                label = { Text("Ghi chú") },
+                modifier = Modifier.fillMaxWidth()
+            )*/
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = {
+                    viewModel.addContact(context)
+                    Log.d("ket qua them contact:","Ket qua: ${viewModel.addContactResult.value}")
+
+
+                }) {
+                    Text("Lưu")
+                }
+
+                OutlinedButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Text("Hủy")
+                }
+            }
+        }
+    }
+}
